f28a414aca62ef0d805f78fe1585c716a56a219a
GerritHillebrecht <52440984+GerritHillebrecht@users.noreply.github.com>
2023-02-05 21:38:53 +0100

    added openAI response

--- src/whatsapp/service/message/message.service.ts
+++ src/whatsapp/service/message/message.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '@user/entity';
 import { UserService } from '@user/service';
 import { Message } from '@whatsapp/entity';
 import { Repository, In, FindManyOptions } from 'typeorm';
 import { PubSub } from 'graphql-subscriptions';
+import { Configuration, CreateCompletionRequest, OpenAIApi } from 'openai';
 
 const pubSub = new PubSub();
 
@@ -17,10 +17,15 @@ type MessageQueryOptions = {
 
 @Injectable()
 export class MessageService {
+  apiKey = process.env.OPENAI_API_KEY;
+  openAiConfiguration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  openai = new OpenAIApi(this.openAiConfiguration);
+
   constructor(
     @InjectRepository(Message)
     private readonly messageRepository: Repository<Message>,
-
     private userService: UserService,
   ) {}
 
@@ -36,9 +41,10 @@ export class MessageService {
     return pubSub.asyncIterator<Message>('whatsappReadUpdate');
   }
 
-  publishReadUpdate(messageIds: number[]) {
+  publishReadUpdate(messages: Message[]) {
+    console.log('PUSBLISHING');
     pubSub.publish('whatsappReadUpdate', {
-      readupdateSubscription: messageIds,
+      readupdateSubscription: messages,
     });
   }
 
@@ -60,6 +66,14 @@ export class MessageService {
       id: newMessage.id,
     });
 
+    if (message.receiver.isBot) {
+      this.openAIResponse({
+        userId: message.sender.id,
+        prompt: message.body,
+      });
+      this.updateReadStatus([message.id]);
+    }
+
     this.publishMessage(message);
     return message;
   }
@@ -79,9 +93,12 @@ export class MessageService {
       { deliveryStatus: 'read' },
     );
 
-    this.publishReadUpdate(Ids);
+    const messages = await this.messageRepository.find({
+      where: { id: In(Ids) },
+    });
 
-    return Ids;
+    this.publishReadUpdate(messages);
+    return messages.map(({ id }) => id);
   }
 
   async deleteMessage(id: number) {
@@ -126,5 +143,30 @@ export class MessageService {
       take: limit,
       skip: offset,
     };
+  }
+
+  private async openAIResponse({
+    userId,
+    prompt,
+  }: {
+    userId: number;
+    prompt: string;
+  }) {
+    console.log('FETCHTING BOT RESPONSE', userId, prompt);
+
+    const completion = await this.openai.createCompletion({
+      model: 'text-davinci-003' as CreateCompletionRequest['model'],
+      prompt,
+      temperature: 0.6,
+      max_tokens: 1000,
+    } as CreateCompletionRequest);
+    const response = completion.data.choices[0].text;
+    console.log({ response });
+    this.saveMessage(
+      'BOT_RESPONSE' + new Date().getTime(),
+      response,
+      userId,
+      18,
+    );
   }
 }
